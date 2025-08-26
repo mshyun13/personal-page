b4a719677bbf7b4f20925c3e1b66ab5d734ca532
Seunghyun Min <markmin1316@gmail.com>
2025-08-27 00:20:45 +1200

    edit: add github & linkedin icons

--- client/components/AboutMe.tsx
+++ client/components/AboutMe.tsx
@@ -4,6 +4,18 @@ function AboutMe () {
       <div className="example">
         <span>{`I hold a Bachelor of Science in Computer Science and Information Systems, with a focus on database studies. I previously worked as a sales support and pricing analyst for 18 months at PB Tech. Recently, I completed a full-time web development bootcamp at Dev Academy, gaining practical experience in full-stack development.`}</span><br />
         <span>{`Moving into the tech industry has long been my ultimate goal, and combining the knowledge from my university studies with the hands-on learning at Dev Academy has strengthened my commitment to this path. With strong communication and human skills, I'm eager to take on new challenges in this field and to develop creative, collaborative solutions to complex problems.`}</span>
+        <ul className="exampleIcon">
+          <li className="githubIcon">
+            <a href={'https://www.linkedin.com/in/seunghyun-min-b55325269/'}>
+              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" alt="github icon" />
+            </a>
+          </li>
+          <li className="linkedinIcon">
+            <a href={`https://github.com/mshyun13`}>
+              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linkedin/linkedin-plain.svg" alt="linkedinIcon" />
+            </a>
+          </li>
+        </ul>
       </div>
     </>
   )
