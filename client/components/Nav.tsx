df9e14dcccd0597ab52b26da99e066cf9ef9c372
Seunghyun Min <markmin1316@gmail.com>
2025-09-02 03:04:21 +1200

    edit: add haslink to nav bar

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,4 +1,4 @@
-// import { useRef } from "react"
+import { HashLink } from 'react-router-hash-link';
 
 function Nav() {
 
@@ -6,9 +6,9 @@ function Nav() {
     <>
       <div className="navWrapper">
         <ul className="navbar">
-          <li><a href="#section1" className="active">MIN SEUNGHYUN</a></li>
-          <li><a href="#section2">PROJECTS</a></li>
-          <li><a href="#section3">ABOUT ME</a></li>
+          <li><HashLink to="#section1" className="active">MIN SEUNGHYUN</HashLink></li>
+          <li><HashLink to="#section2">PROJECTS</HashLink></li>
+          <li><HashLink to="#section3">ABOUT ME</HashLink></li>
         </ul>
       </div>
     </>
